da50420b97a9a9814dbaa2cacef94735a8052963
Shubham <salunkheshubham0@gmail.com>
2022-01-28 15:39:13 +0530

    Revert "UI update - margin change"
    This reverts commit 40c5564a9b2f9bef0a9ded2bf12f99ec1cfa38e0.

--- src/Components/ButtonGroupMUI.js
+++ src/Components/ButtonGroupMUI.js
@@ -11,7 +11,7 @@ const ButtonGroupMUI = () => {
           display: "flex",
           justifyContent: "center",
           backgroundColor: "#e5e0d9",
-          m: "15",
+          m: "10",
         }}
       >
         <Stack direction="row" spacing={2}>
