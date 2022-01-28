4a81f4558d09266acd92026a0e9815f4d8746ad9
Shubham <salunkheshubham0@gmail.com>
2022-01-28 14:15:42 +0530

    test

--- src/Components/AutocompleteMUI.js
+++ src/Components/AutocompleteMUI.js
@@ -38,8 +38,6 @@ const top100Films = [
   { label: "The Godfather: Part II", year: 1974 },
   { label: "The Dark Knight", year: 2008 },
   { label: "12 Angry Men", year: 1957 },
-  { label: "Schindler's List", year: 1993 },
-  { label: "Pulp Fiction", year: 1994 },
   {
     label: "The Lord of the Rings: The Return of the King",
     year: 2003,
